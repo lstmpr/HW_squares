8bb846d99cc75c5ff0f9874d19858178a8677dd3
Ekaterina Shilova <ekaterinalstmpr@gmail.com>
2021-09-26 14:49:10 +0300

    rearrange entries

--- Squares/src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ Squares/src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -1,6 +1,7 @@
 package ru.netology.sqr;
 
 //import org.junit.jupiter.api.Test;
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -10,9 +11,9 @@ class SQRServiceTest {
 
     @ParameterizedTest
     @CsvSource(value = {"'count squares from bottom line to upper bound', 200, 300, 3",
-    "'count squares when bottom line equals upper bound', 225, 225, 1",
-    "'count Squares When bottom line and upper bound less ten', 1, 9, 0",
-    "'count Squares when bottom line more upper bound', 300, 200, 0"})
+            "'count squares when bottom line equals upper bound', 225, 225, 1",
+            "'count Squares When bottom line and upper bound less ten', 1, 9, 0",
+            "'count Squares when bottom line more upper bound', 300, 200, 0"})
     void countSquares(String testName, int bottomLine, int upperBound, int expected) {
         SQRService service = new SQRService();
 //
@@ -20,10 +21,10 @@ class SQRServiceTest {
 //        int upperBound = 300;
 //        int expected = 3;
 //
-       int result = service.countSquares(bottomLine, upperBound);
+        int result = service.countSquares(bottomLine, upperBound);
 //
-       assertEquals(expected, result);
-   }
+        assertEquals(expected, result);
+    }
 //
 //    @Test
 //    void countSquaresWhenBottomLineEqualsUpperBound() {
